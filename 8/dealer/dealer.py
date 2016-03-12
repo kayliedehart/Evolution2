7d77744bf078871adaf0124b37551998a470d4ca
Kaylie DeHart <kaylie.dehart@gmail.com>
2016-03-12 16:54:38 -0500

    dealer feed1 method

--- 8/dealer/dealer.py
+++ 8/dealer/dealer.py
@@ -1,14 +1,119 @@
 # Representation of the dealer in a game of Evolution
+import species
+import player
+
 class Dealer:
-	wateringHole = []
+	wateringHole = 0
 	players = []
 	deck = []
+	discard = []
 
 	def __init__(self):
 		pass
 
+	def feedFromWateringHole(self, player, species, foodCount):
+		if species.hasTrait("foraging"):
+			foodCount += 1
+		if self.wateringHole >= foodCount:
+			species.food += foodCount
+			self.wateringHole -= foodCount
+		else:
+			species.food += self.wateringHole
+			self.wateringHole = 0
+
+		left, right = Player.getNeighbors(player, species)
+
+		if species.hasTrait("cooperation"):
+			self.feedFromWateringHole(player, right, 1)
+
 	"""
-	Execute the next step in the feeding routine
+	Execute a carnivore attack, including feeding
 	"""
-	def feed1(self):
-		pass
+	def executeAttack(self, attPlayer, defend, att):
+		defend.population -= 1
+		if defend.hasTrait("horns"):
+			att.population -= 1
+		if att.population > att.food:
+			self.feedFromWateringHole(attPlayer, att, 1)
+
+
+	"""
+	Try to automatically feed a species of the given player. If there is only one remaining
+	hungry species, and it's a herbivore, it can be automatically fed.
+	Return True on success.
+	@param player: the current PlayerState
+	"""
+	def autoFeed(self, player):
+		hungry = []
+		for s in player.species:
+			if not (s.population > s.food or (s.body > s.fatFood and s.hasTrait("fat-tissue"))):
+				hungry.append(s)
+
+		if (len(hungry) == 1) and not (hungry[0].hasTrait("carnivore")):
+			# if the only species is a herbivore but has unfilled fat-tissue, must query
+			if hungry[0].hasTrait("fat-tissue") and hungry[0].body > hungry[0].fatFood:
+				return False
+			else:
+				self.feedFromWateringHole(player, hungry[0], 1)
+				return True
+		return False
+
+	"""
+	Query the given player for what species to feed next, and feed according to 
+	response. Return whether the query resulted in a successful attack. 
+	@param player: the current PlayerState
+	@return Boolean: if feeding took place
+	"""
+	def queryFeed(self, player):
+		decision = Player.feed(player, self.wateringHole, self.players)
+		if decision:
+			if type(decision) == int:
+				self.feedFromWateringHole(player, player.species[decision], 1)
+				return False
+			if len(decision) == 2:
+				if self.wateringHole >= decision[1]:
+					player.species[decision[0]].fatFood += decision[1]
+					self.wateringHole -= decision[1]
+				else:
+					player.species[decision[0]].fatFood += self.wateringHole
+					self.wateringHole = 0
+				return False
+			if len(decision) == 3:
+				attacker = player.species[decision[0]]
+				defender = self.players[decision[1]]
+				prey = defender.species[decision[2]]
+				left, right = Player.getNeighbors(defender, prey)
+				if Species.isAttackable(defender, attacker, left, right):
+					self.executeAttack(player, defender, attacker)
+					return True
+		else:
+			return False
+
+	"""
+	Execute automatic feedings triggered by scavenging traits.
+	@param player: current PlayerState
+	"""
+	def scavengeFeed(self, player):
+		while self.wateringHole > 0:
+			for spec in player.species:
+				if spec.hasTrait("cooperation")
+				# if attack, scavenging
+
+
+	"""
+	Execute the next step in the feeding routine; either feeding the next player automatically
+	or querying the player for their feeding decision, and completing subsequent triggered feedings
+
+	#Invariants: wateringHole must be greater than 0
+	@param configuration: a list of PlayerStates
+	"""
+	def feed1(self, configuration):
+		curPlayer = configuration[0]
+		if wateringHole > 0:
+			if not self.autoFeed(curPlayer):
+				attack = self.queryFeed(curPlayer)
+				if attack:
+					self.scavengeFeed(curPlayer)
+
+	#TODO: iterate through Players and call helper feed1
+	# remove Players from list if they have no hungry species
