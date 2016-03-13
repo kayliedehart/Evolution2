eff82a146a27cb18d639a44dd4aa090f590afac8
Phaelyn Kotuby <kotubyp@ccs.neu.edu>
2016-03-12 19:37:14 -0500

    forgot to add xstep test harness

---
+++ 8/test_xstep.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2.7
+
+import sys
+import subprocess
+
+TEST_PATH = "json_tests/"
+PYTHON2_PATH = "python2.7"
+
+
+def xstep(testName, fileName):
+    printing = testName + "\n\tresult: "
+    try:
+        result = subprocess.check_output([PYTHON2_PATH, sys.path[0] + "/xstep"], stdin=fileName)
+        if not result:
+            printing += "program quit\n"
+        else:
+            printing += result
+    except Exception as e:
+        printing += str(e)
+
+    print printing
+
+
+with open(TEST_PATH + "/xstep_in_1.json", "r") as one:
+    xstep("regular configuration; expected output: one configuration", one)
+
+with open(TEST_PATH + "/xstep_invalid.json", "r") as invalid:
+    xstep("bad json; expected output: none", invalid)	
+
+with open(TEST_PATH + "/xstep_invalid_players.json", "r") as invalidPlayers:
+    xstep("invalid json players; expected output: none", invalidPlayers)	
