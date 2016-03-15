b1935a9351d2a91c7b4b0d36e23a6f719551a964
Phaelyn Kotuby <kotubyp@ccs.neu.edu>
2016-03-15 15:37:02 -0400

    Draws an entire player...but vertically

--- 9/gui/drawing.py
+++ 9/gui/drawing.py
@@ -12,7 +12,7 @@ class Drawing:
 	def __init__(self, root, dealer=None, player=None):	
 		self.exTrait = TraitCard("carnivore", 5)
 		self.exTrait2 = TraitCard("fat-tissue", 3)
-		self.exSpecies = Species(2, 3, 4, [self.exTrait, self.exTrait2], 0)
+		self.exSpecies = Species(2, 3, 4, ["fat-tissue", "foraging"], 0)
 		self.exPlayer = PlayerState(1, 3, [self.exSpecies], [self.exTrait])
 		#self.exDealer= Dealer([self.exPlayer], 10, [self.exTrait, self.exTrait])
 		player = self.exPlayer
@@ -31,16 +31,14 @@ class Drawing:
 		playerFrame = LabelFrame(master, text=ptext, padx=10, pady=10)
 		playerFrame.pack()
 
-		foodBagFrame = LabelFrame(playerFrame, text="Food Bag", padx=3, pady=3)
-		foodBagFrame.pack()
-		foodBagLabel = Label(foodBagFrame, text=str(player.foodbag))
-		foodBagLabel.pack()
+		self.makeLabelFrame(playerFrame, "Food Bag", player.foodbag)
 
 		for species in player.species:
 			self.drawSpecies(playerFrame, species)
 
 		handFrame = LabelFrame(playerFrame, text="Hand", padx=10, pady=10)
 		handFrame.pack()
+
 		for card in player.hand:
 			self.drawTraitCard(handFrame, card)
 
@@ -53,25 +51,19 @@ class Drawing:
 		foodFrame = self.drawFood(speciesFrame, species.food)
 		foodFrame.pack()
 
-		bodyFrame = LabelFrame(speciesFrame, text="Body")
-		bodyFrame.pack()
-		bodyLabel = Label(master=bodyFrame, text=species.body)
+		self.makeLabelFrame(speciesFrame, "Body", species.body)
+		self.makeLabelFrame(speciesFrame, "Population", species.population)
 
-		popFrame = LabelFrame(speciesFrame, text="Population")
-		popFrame.pack()
-		popLabel = Label(master=popFrame, text=species.population)
+		if species.hasTrait("fat-tissue") and species.fatFood > 0:
+			self.makeLabelFrame(speciesFrame, "Fat Food", species.fatFood)
 
-		if species.hasTrait("fat-tissue"):
-			fatFoodFrame = LabelFrame(speciesFrame, text="Fat Food")
-			fatFoodFrame.pack()
-			fatFoodLabel = Label(master=fatFoodFrame, text=species.fatFood)
+		for trait in species.traits:
+			self.makeLabelFrame(speciesFrame, "Trait", trait)
 
-		for card in species.traits:
-			self.drawTraitCard(speciesFrame, card)
-
+		return speciesFrame
 
 	def drawFood(self, master, food):
-		foodFrame = LabelFrame(master, text="FOOD", padx=10, pady=10)
+		foodFrame = LabelFrame(master, text="Food", padx=10, pady=10)
 		foodFrame.pack()
 
 		photo = PhotoImage(file="ham.gif")
@@ -86,18 +78,16 @@ class Drawing:
 		return foodFrame
 
 	def drawTraitCard(self, master, card):
-		traitFrame = LabelFrame(master, text="TRAIT", padx=10, pady=10)
-		traitFrame.pack()
-
 		label = card.name + ", " + str(card.food) 
-		labelTrait = Label(master=traitFrame, text=label)
-		labelTrait.pack()
+		self.makeLabelFrame(master, "Trait", label)
 
-
+	def makeLabelFrame(self, master, titleText, bodyText):
+		titleFrame = LabelFrame(master=master, text=titleText, padx=10, pady=10)
+		titleFrame.pack()
+		bodyLabel = Label(master=titleFrame, text=str(bodyText))
+		bodyLabel.pack()
 
 
 root = Tk()
-
 drawing = Drawing(root)
-
 root.mainloop()
