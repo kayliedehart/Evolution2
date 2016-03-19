967524cefea9023f6d714df4892a29f9770a4243
Phaelyn Kotuby <kotubyp@ccs.neu.edu>
2016-03-19 17:13:15 -0400

    Should be ready to plug into a controller. Scrollbars work, code is commented and abstracted as best as possible

--- 9/gui/drawing.py
+++ 9/gui/drawing.py
@@ -11,7 +11,7 @@ class Drawing:
 		or don't
 		i'm a comment, not a cop
 	"""
-	def __init__(self, root, dealer=None, player=None):	
+	def __init__(self, dealer=None, player=None):	
 		self.exTrait = TraitCard("carnivore", 5)
 		self.exTrait2 = TraitCard("fat-tissue", 3)
 		self.exTrait3 = TraitCard("long-neck", 1)
@@ -23,65 +23,82 @@ class Drawing:
 		self.exPlayer = PlayerState(1, 3, [self.exSpecies], [self.exTrait])
 		self.exPlayer2 = PlayerState(2, 5, [self.exSpecies2, self.exSpecies3], [self.exTrait2, self.exTrait3])
 		self.exDealer= Dealer([self.exPlayer, self.exPlayer2], 10, [self.exTrait, self.exTrait2, self.exTrait3, self.exTrait4, self.exTrait5])
-		#player = self.exPlayer
+		player = self.exPlayer
 		dealer = self.exDealer
 
 		# real constructor starts here
+		root = Tk()
 		self.canvas = Canvas(root, width=800, height=600)
+		self.dealerMaster = self.playerMaster = None
 
 		if dealer is not None:
 			self.dealerMaster = self.drawDealer(self.canvas, dealer)
 			self.dealerMaster.grid(row=0, column=0)
-		if player is not None:
+			self.createScrollbars(root, self.dealerMaster)
+		elif player is not None:
 			self.playerMaster = self.drawPlayer(self.canvas, player)
 			self.playerMaster.grid(row=0, column=0)
+			self.createScrollbars(root, self.playerMaster)
 		if dealer is None and player is None:
 			raise ValueError("Must give a dealer or a player")
 
-		self.vertiscroll = Scrollbar(root, orient="vertical", command=self.canvas.yview)
-		self.horizscroll = Scrollbar(root, orient="horizontal", command=self.canvas.xview)
-		self.canvas.configure(yscrollcommand=self.vertiscroll.set, xscrollcommand=self.horizscroll.set)
-		self.vertiscroll.pack(side="right", fill="y")
-		self.horizscroll.pack(side="bottom", fill="x")
-		self.canvas.pack()
-		self.canvas.create_window((0, 0,), window=self.dealerMaster, anchor="nw")
-		self.dealerMaster.bind("<Configure>", self.rootFrameConfigure)
+		root.mainloop()
 
+
+	"""
+		Create scrollbars for the window
+		LabelFrame or None, LabelFrame or None -> Void
+	"""
+	def createScrollbars(self, root, master):
+		vertiscroll = Scrollbar(root, orient="vertical", command=self.canvas.yview)
+		horizscroll = Scrollbar(root, orient="horizontal", command=self.canvas.xview)
+		self.canvas.configure(yscrollcommand=vertiscroll.set, xscrollcommand=horizscroll.set)
+		vertiscroll.pack(side="right", fill="y")
+		horizscroll.pack(side="bottom", fill="x")
+		self.canvas.pack()
+		self.canvas.create_window((0, 0,), window=master, anchor="nw")
+		master.bind("<Configure>", self.rootFrameConfigure)
+
+	"""
+		Event handler to move the canvas when a scrollbar is moved
+		Event -> Void
+	"""
 	def rootFrameConfigure(self, event):
 		self.canvas.configure(scrollregion=self.canvas.bbox("all"))		
 
+	"""
+		Create a LabelFrame containing representations of everything within the Dealer
+		Tk, Dealer, Opt: Nat, Opt:Nat -> LabelFrame
+	"""
 	def drawDealer(self, master, dealer, row=0, column=0):
 		dealerFrame = LabelFrame(master, text="Dealer", padx=10, pady=10)
 		dealerFrame.grid(row=row, column=column)
 
-		self.makeLabelFrame(dealerFrame, "Watering Hole", dealer.wateringHole, row=row, column=column)
-
-		deckText = str(len(dealer.deck)) + " cards left"
-		self.makeLabelFrame(dealerFrame, "Deck", deckText, row=row+1, column=column)
+		self.makeLabelFrame(dealerFrame, "Watering Hole", "{} tokens".format(dealer.wateringHole), row=row, column=column)
+		self.makeLabelFrame(dealerFrame, "Deck", "{} cards left".format(len(dealer.deck)), row=row+1, column=column)
 
 		column += 1
 
 		playersFrame = LabelFrame(dealerFrame, text="Players", padx=10, pady=10)
 		playersFrame.grid(row=row, column=column, rowspan=len(dealer.players))
-
 		for i in range(len(dealer.players)):
 			self.drawPlayer(playersFrame, dealer.players[i], row=row+i, column=column)
 
 		return dealerFrame
 
-
+	"""
+		Create a LabelFrame containing representations of everything within the Player
+		Tk, Player, Opt: Nat, Opt:Nat -> LabelFrame
+	"""
 	def drawPlayer(self, master, player, row=0, column=0):
 		ptext = "Player " + str(player.num)
 		playerFrame = LabelFrame(master, text=ptext, padx=10, pady=10)
 		playerFrame.grid(row=row, column=column, sticky="nw")
 
-		playerCanvas = Canvas(master, width=300, height=400)
-
-		self.makeLabelFrame(playerFrame, "Food Bag", player.foodbag, row=row, column=column)
+		self.makeLabelFrame(playerFrame, "Food Bag", "{} tokens".format(player.foodbag), row=row, column=column)
 		
 		speciesFrame = LabelFrame(playerFrame, text="Species List", padx=10, pady=10)
 		speciesFrame.grid(row=row, column=column+1)
-
 		for i in range(len(player.species)):
 			self.drawSpecies(speciesFrame, player.species[i], row=row, column=column+i+2)
 		
@@ -89,12 +106,15 @@ class Drawing:
 
 		handFrame = LabelFrame(playerFrame, text="Hand", padx=10, pady=10)
 		handFrame.grid(row=row, column=column)
-
 		for i in range(len(player.hand)):
 			self.drawTraitCard(handFrame, player.hand[i], row=row, column=column+i)
 		
 		return playerFrame
 
+	"""
+		Create a LabelFrame containing representations of everything within the Species
+		Tk, Species, Opt: Nat, Opt:Nat -> LabelFrame
+	"""
 	def drawSpecies(self, master, species, row=0, column=0):
 		speciesFrame = LabelFrame(master, text="Species", padx=10, pady=10)
 		speciesFrame.grid(row=row, column=column)
@@ -107,22 +127,28 @@ class Drawing:
 		foodFrame = self.drawFood(speciesFrame, species.food)
 		foodFrame.grid(row=row, column=column)
 
-		self.makeLabelFrame(speciesFrame, "Body", species.body, row=row, column=column+1)
-		self.makeLabelFrame(speciesFrame, "Population", species.population, row=row, column=column+2)
-
+		column += 1
+		self.makeLabelFrame(speciesFrame, "Body", species.body, row=row, column=column)
+		column += 1
+		self.makeLabelFrame(speciesFrame, "Population", species.population, row=row, column=column)
+		column += 1
 		if species.hasTrait("fat-tissue") and species.fatFood > 0:
-			self.makeLabelFrame(speciesFrame, "Fat Food", species.fatFood, row=row, column=column+3)
-
+			self.makeLabelFrame(speciesFrame, "Fat Food", species.fatFood, row=row, column=column)
 
 		return speciesFrame
 
+
+	"""
+		Draw the food a Species has with an illustration
+		Tk, Nat, Opt: Nat, Opt: Nat -> LabelFrame
+	"""
 	def drawFood(self, master, food, row=0, column=0):
 		foodFrame = LabelFrame(master, text="Food", padx=10, pady=10)
 		foodFrame.grid(row=row, column=column)
 
 		photo = PhotoImage(file="ham.gif")
 		ham = Label(master=foodFrame, image=photo)
-		ham.image = photo
+		ham.image = photo # save a reference
 		ham.grid(row=row, column=column)
 
 		xNum = "x" + str(food)
@@ -131,17 +157,23 @@ class Drawing:
 
 		return foodFrame
 
+	"""
+		Create a LabelFrame containing TraitCard
+		Tk, TraitCard, Opt: Nat, Opt:Nat -> LabelFrame
+	"""
 	def drawTraitCard(self, master, card, row=0, column=0):
-		label = card.name + ", " + str(card.food) 
-		self.makeLabelFrame(master, "Trait", label, row, column)
+		self.makeLabelFrame(master, "Trait", "{}, {}".format(card.name, card.food), row, column)
 
+	"""
+		Create a LabelFrame with a title, then draw a label with contents inside that LabelFrame
+		Tk, String, String, Opt: Nat, Opt:Nat -> LabelFrame
+	"""
 	def makeLabelFrame(self, master, titleText, bodyText, row=0, column=0):
 		titleFrame = LabelFrame(master=master, text=titleText, padx=10, pady=10)
 		titleFrame.grid(row=row, column=column, sticky="NEWS")
 		bodyLabel = Label(master=titleFrame, text=str(bodyText))
 		bodyLabel.grid(row=row+1, column=column)
 
+if __name__ == "__main__":
+	Drawing()
 
-root = Tk()
-drawing = Drawing(root)
-root.mainloop()
